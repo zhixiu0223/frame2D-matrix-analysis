2337622b1ad808e9b24127656787689f15e82061
zhixiu0223 <zhixiu0223@gmail.com>
2026-08-29 18:34:06 +0800

    Remove old output_plots files

--- examples/output_plots/frame2d/__init__.py
+++
@@ -1,7 +0,0 @@
-from .model import Frame2D, Node, Section, Member, Support, PointLoad, DistributedLoad
-from .solve import solve, SolveResult, MemberResult
-
-__all__ = [
-    "Frame2D", "Node", "Section", "Member", "Support", "PointLoad", "DistributedLoad",
-    "solve", "SolveResult", "MemberResult",
-]
